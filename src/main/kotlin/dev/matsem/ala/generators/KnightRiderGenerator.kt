e8046a1dac0f4a91a63fba7bc5f4068f3e24e875
matsem <matej.semancik@gmail.com>
2020-04-07 23:48:50 +0200

    Fade PGraphics using bitwise operations

--- src/main/kotlin/dev/matsem/ala/generators/KnightRiderGenerator.kt
+++ src/main/kotlin/dev/matsem/ala/generators/KnightRiderGenerator.kt
@@ -15,13 +15,10 @@ class KnightRiderGenerator(private val sketch: PApplet, w: Int, h: Int) {
             colorMode(PConstants.HSB, 360f, 100f, 100f, 100f)
             pushPop {
                 rectMode(PConstants.CORNER)
-
-                noStroke()
-                fill(0f, 0f, 0f, fading * 100f)
-                rect(0f, 0f, width.toFloat(), height.toFloat())
-
                 noStroke()
                 fill(color)
+
+                fadeToBlackBy(fading)
                 val x = PApplet.sin(sketch.angularTimeHz(fHz)).mapSin(0f, width.toFloat()).toInt()
                 rect(x.toFloat(), 0f, w.constrain(low = 1).toFloat(), height.toFloat())
             }
--- src/main/kotlin/dev/matsem/ala/tools/extensions/PGraphicsExtensions.kt
+++ src/main/kotlin/dev/matsem/ala/tools/extensions/PGraphicsExtensions.kt
@@ -27,3 +27,20 @@ fun PGraphics.draw(block: PGraphics.() -> Unit) {
     this.block()
     endDraw()
 }
+
+/**
+ * Fades whole image to complete black by [fadeAmount] in range 0f..1f
+ */
+fun PGraphics.fadeToBlackBy(fadeAmount: Float) {
+    loadPixels()
+    for (i in 0 until pixels.count()) {
+        val r = pixels[i].rgbRed
+        val newR = max(0, (r * fadeAmount).toInt())
+        val g = pixels[i].rgbGreen
+        val newG = max(0, (g * fadeAmount).toInt())
+        val b = pixels[i].rgbBlue
+        val newB = max(0, (b * fadeAmount).toInt())
+        pixels[i] = (0xff shl 24) or (newR shl 16) or (newG shl 8) or newB
+    }
+    updatePixels()
+}
